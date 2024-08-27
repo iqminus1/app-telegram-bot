5bc877163aa030b7efcf1cb968a4207c86fd6c03
Abdulaziz <wwxeww1wwxeww@gmail.com>
2024-08-27 15:23:05 +0500

    fixed bug

--- src/main/java/uz/pdp/apptelegrambot/service/admin/ChatJoinRequestServiceImpl.java
+++ src/main/java/uz/pdp/apptelegrambot/service/admin/ChatJoinRequestServiceImpl.java
@@ -5,6 +5,7 @@ import org.telegram.telegrambots.meta.api.objects.ChatJoinRequest;
 import uz.pdp.apptelegrambot.entity.Order;
 import uz.pdp.apptelegrambot.enums.LangEnum;
 import uz.pdp.apptelegrambot.enums.LangFields;
+import uz.pdp.apptelegrambot.repository.GroupRepository;
 import uz.pdp.apptelegrambot.repository.OrderRepository;
 import uz.pdp.apptelegrambot.service.LangService;
 import uz.pdp.apptelegrambot.service.admin.bot.AdminSender;
@@ -19,12 +20,17 @@ public class ChatJoinRequestServiceImpl implements ChatJoinRequestService {
     private final OrderRepository orderRepository;
     private final LangService langService;
     private final AdminResponseButton adminResponseButton;
+    private final GroupRepository groupRepository;
     private final AdminUtils adminUtils;
 
     @Override
     public void process(ChatJoinRequest chatJoinRequest) {
-        Long userId = chatJoinRequest.getUser().getId();
         Long groupId = chatJoinRequest.getChat().getId();
+        if (groupRepository.findByGroupId(groupId).isEmpty()) {
+            return;
+        }
+
+        Long userId = chatJoinRequest.getUser().getId();
         Optional<Order> optionalOrder = orderRepository.findByUserIdAndGroupId(userId, groupId);
         if (optionalOrder.isPresent()) {
             if (optionalOrder.get().getExpireDay().after(new Date())) {
